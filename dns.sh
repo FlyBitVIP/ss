f8a1630e1869b0fbbd2cf9421337fc14ec514874
FlyBitVIP <kabsrsynabsrst@gmail.com>
2024-09-12 23:47:00 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -45,8 +45,8 @@ fi
 # 下载文件
 curl -o /home/tempdns.txt $TEMP_URL
 
-# 替换文件中的 {{DNS_TARGET_URL}} 占位符
-sed -e "s|{{URL}}|$DNS_URL|g" /home/tempdns.txt > $OUTPUT_FILE
+# 替换文件中的 {{URL}} 占位符
+sed -e "s/URL/$DNS_URL/g" /home/tempdns.txt > $OUTPUT_FILE
 
 # 显示结果
 echo "文件已生成: $OUTPUT_FILE"
